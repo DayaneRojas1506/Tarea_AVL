cc44044f091c0e7ba2885c9e972ee28ff1b086f3
Dayane Rojas <brigitte.rojas@utec.edu.pe>
2024-01-25 00:06:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,45 +5,45 @@ using namespace std;
 
 int main() {
 
-  BSTree<int> BST;
-  AVLTree<int> AVL;
+    BSTree<int> BST;
+    AVLTree<int> AVL;
 
- int array[] = {10, 5, 20, 2, 8, 15, 30};
- // int array2[] = {10, 5, 20, 2, 8, 15, 30};
+    int array[] = {10, 5, 20, 2, 8, 15, 30};
+    int array2[] = {10, 5, 20, 2, 8, 15, 30};
 
-  cout << "    ==========================     " << endl;
-  cout << "----|     **   BST   **      |-----" << endl;
-  cout << "    ==========================     " << endl;
+    cout << "    ==========================     " << endl;
+    cout << "----|     **   BST   **      |-----" << endl;
+    cout << "    ==========================     " << endl;
 
-  for (int i = 0; i < 7; i++) {
-    cout << "Insertando nodo : " << array[i] << endl;
-    BST.insert(array[i]);
-    cout << "==  Arbol actual  ==" << endl;
-    BST.print();
-    cout << "====================" << endl;
-  }
+    for (int i = 0; i < 7; i++) {
+        cout << "Insertando nodo : " << array[i] << endl;
+        BST.insert(array[i]);
+        cout << "==  Arbol actual  ==" << endl;
+        BST.print();
+        cout << "====================" << endl;
+    }
 
-  cout << "- MinValue : " << BST.minValue() << endl;
-  cout << "- MaxValue : " << BST.maxValue() << endl;
-  cout << "- Recorrido InOrder : " << BST.inOrder() << endl;
-  cout << "- Recorrido PostOrder : " << BST.postOrder() << endl;
-  cout << "- Recorrido PreOrder : " << BST.preOrder() << endl;
-  cout << "- Sucesor de 8 : " << BST.successor(8) << endl;
-  cout << "- Predecesor de 5 : " << BST.predecessor(5) << endl;
-  cout << "- "; BST.BreadthFirstSearch();
-  cout << "- "; BST.DepthFirstSearch();
-  cout << "=====================" << endl;
-  cout << "- Eliminando nodo 8 " << endl;
+    cout << "- MinValue : " << BST.minValue() << endl;
+    cout << "- MaxValue : " << BST.maxValue() << endl;
+    cout << "- Recorrido InOrder : " << BST.inOrder() << endl;
+    cout << "- Recorrido PostOrder : " << BST.postOrder() << endl;
+    cout << "- Recorrido PreOrder : " << BST.preOrder() << endl;
+    cout << "- Sucesor de 8 : " << BST.successor(8) << endl;
+    cout << "- Predecesor de 5 : " << BST.predecessor(5) << endl;
+    cout << "- "; BST.BreadthFirstSearch();
+    cout << "- "; BST.DepthFirstSearch();
+    cout << "=====================" << endl;
+    cout << "- Eliminando nodo 8 " << endl;
     BST.remove(8);
-  BST.print();
-  cout << "=====================" << endl;
+    BST.print();
+    cout << "=====================" << endl;
 
-  cout << "- Eliminando nodo 20 (Se reemplaza por el predecesor) " << endl;
-   BST.remove(20);
-  BST.print();
-  cout << "=====================" << endl;
+    cout << "- Eliminando nodo 20 (Se reemplaza por el predecesor) " << endl;
+    BST.remove(20);
+    BST.print();
+    cout << "=====================" << endl;
 
-/*
+
   cout << "    ==========================     " << endl;
   cout << "----|     **   AVL   **      |-----" << endl;
   cout << "    ==========================     " << endl;
@@ -55,6 +55,24 @@ int main() {
     AVL.print();
     cout << "====================" << endl;
   }
-  */
-return 0;
+    cout << "- MinValue : " << AVL.minValue() << endl;
+    cout << "- MaxValue : " << AVL.maxValue() << endl;
+    cout << "- Recorrido InOrder : " << AVL.inOrder() << endl;
+    cout << "- Recorrido PostOrder : " << AVL.postOrder() << endl;
+    cout << "- Recorrido PreOrder : " << AVL.preOrder() << endl;
+    cout << "- Sucesor de 8 : " << AVL.successor(8) << endl;
+    cout << "- Predecesor de 5 : " << AVL.predecessor(5) << endl;
+   
+    cout << "=====================" << endl;
+    cout << "- Eliminando nodo 8 " << endl;
+    AVL.remove(8);
+    AVL.print();
+    cout << "=====================" << endl;
+
+    cout << "- Eliminando nodo 20 (Se reemplaza por el predecesor) " << endl;
+    AVL.remove(20);
+    AVL.print();
+    cout << "=====================" << endl;
+
+    return 0;
 }
